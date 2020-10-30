51bbebde9f2a5a958853b037276cfcb212719d0c
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-30 06:01:25 -0600

    Initial commit

---
+++ src/GreenTicket2.java
@@ -0,0 +1,11 @@
+public class GreenTicket2 {
+  public int greenTicket(int a, int b, int c) {
+    if (a == b && b == c) {
+      return 20;
+    } else if (a != b && b != c && a != c) {
+      return 0;
+    } else {
+      return 10;
+    }
+  }
+}
